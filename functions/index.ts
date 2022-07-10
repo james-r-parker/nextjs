6193e9bdd39d9eea36f00e281f74229f8ec381b1
James Parker <j1mb0jay@jimbojay.com>
2022-07-10 13:06:51 +0100

    multiple cookies

--- functions/index.ts
+++ functions/index.ts
@@ -6,11 +6,11 @@ const cookies: PagesFunction<{}> = async ({ request, next }) => {
     if (!cookies.includes("P2G.Id=")) {
         const d = new Date();
         d.setFullYear(d.getFullYear() + 1);
-        response.headers.set('Set-Cookie', `P2G.Id=${Math.random().toString(16).substring(2, 32)}; Secure; HttpOnly; Expires=${d.toUTCString()}`);
+        response.headers.append('Set-Cookie', `P2G.Id=${Math.random().toString(16).substring(2, 32)}; Secure; HttpOnly; Expires=${d.toUTCString()}`);
     }
 
     if (!cookies.includes("P2G.Session.Id=")) {
-        response.headers.set('Set-Cookie', `P2G.Session.Id=${Math.random().toString(16).substring(2, 32)}; Secure;`);
+        response.headers.append('Set-Cookie', `P2G.Session.Id=${Math.random().toString(16).substring(2, 32)}; Secure;`);
     }
 
     return response;
