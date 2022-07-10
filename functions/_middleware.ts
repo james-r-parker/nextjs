f864321b4219a04e787e5f824676ec8535754294
James Parker <j1mb0jay@jimbojay.com>
2022-07-10 12:51:08 +0100

    cookies

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,6 +1,18 @@
 const cookies: PagesFunction<{}> = async ({ request, next }) => {
     const response = await next();
-    response.headers.set('X-Hello', 'Hello from functions Middleware!');
+
+    const cookies = request.headers.get("Cookie") || '';
+
+    if (!cookies.includes("P2G.Id=")) {
+        const d = new Date();
+        d.setFullYear(d.getFullYear() + 1);
+        response.headers.set('Set-Cookie', `P2G.Id=${Math.random().toString(16).substring(2, 32)}; Secure; HttpOnly; Expires=${d.toUTCString()}`);
+    }
+
+    if (!cookies.includes("P2G.Session.Id=")) {
+        response.headers.set('Set-Cookie', `P2G.Session.Id=${Math.random().toString(16).substring(2, 32)}; Secure;`);
+    }
+
     return response;
 };
 
